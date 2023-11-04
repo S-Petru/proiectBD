5c3e1e5757f32af5f25b5a2872e4418314793feb
S-Petru <slavilapetru@gmail.com>
2023-11-04 22:21:50 +0200

    show request content attempt

--- proiect_bd/src/TestComponent.js
+++ proiect_bd/src/TestComponent.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 const TestComponent = () => {
-    const [responseData, setResponseData] = useState(null);
-  
-    useEffect(() => {
-// console.log(`localhost:3001/test`)
+  const [responseData, setResponseData] = useState(null);
 
-      const fetchTestData = async () => {
+  useEffect(() => {
+    const fetchTestData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/test`);
+        if (response.ok) {
+          const data = await response.json();
+          setResponseData(data);
+        } else {
+          console.error('Failed to fetch data.');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-        // fetch("http://localhost:3001/test")
-        fetch(`${process.env.REACT_APP_API_URL}/test`)
-.then(response => response.json())
-.then(data => {
-  console.log('Received JSON:', data);
-})
-.catch(error => console.error('Error fetching data:', error));
+    fetchTestData();
+  }, []);
 
-      };
-  
-      fetchTestData();
-    }, []);
-  
-    return (
-      <div>
-        {responseData && (
-          <div>
-            <p>Test data received from backend:</p>
-            <pre>{JSON.stringify(responseData, null, 2)}</pre>
-          </div>
-        )}
-      </div>
-    );
-  };
-  
-  export default TestComponent;
+  return (
+    <div>
+      {responseData && (
+        <div>
+          <p>Test data received from backend:</p>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TestComponent;

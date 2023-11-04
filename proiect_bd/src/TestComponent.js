75bd98c7431f62caf5a7bc9cd62a799cbd1ede7e
S-Petru <slavilapetru@gmail.com>
2023-11-04 22:52:12 +0200

    Show result attempt

--- proiect_bd/src/TestComponent.js
+++ proiect_bd/src/TestComponent.js
@@ -16,22 +16,58 @@ import React, { useState, useEffect } from 'react';
 //       .catch(serviceHelper.onGlobalError);
 //   };
 
+// const TestComponent = () => {
+//     const [responseData, setResponseData] = useState(null);
+  
+//     useEffect(() => {
+// // console.log(`localhost:3001/test`)
+
+//       const fetchTestData = async () => {
+
+//         // fetch("http://localhost:3001/test")
+//         fetch(`${process.env.REACT_APP_API_URL}/test`)
+//             .then(response => response.json())
+//             .then(data => {
+//             console.log('Received JSON:', data);
+// })
+// .catch(error => console.error('Error fetching data:', error));
+
+//       };
+  
+//       fetchTestData();
+//     }, []);
+  
+//     return (
+//       <div>
+//         {responseData && (
+//           <div>
+//             <p>Test data received from backend:</p>
+//             <pre>{JSON.stringify(responseData, null, 2)}</pre>
+//           </div>
+//         )}
+//       </div>
+//     );
+//   };
+
 const TestComponent = () => {
     const [responseData, setResponseData] = useState(null);
+    const [error, setError] = useState(null);
   
     useEffect(() => {
-// console.log(`localhost:3001/test`)
-
       const fetchTestData = async () => {
-
-        // fetch("http://localhost:3001/test")
-        fetch(`${process.env.REACT_APP_API_URL}/test`)
-            .then(response => response.json())
-            .then(data => {
-            console.log('Received JSON:', data);
-})
-.catch(error => console.error('Error fetching data:', error));
-
+        try {
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/test`);
+          if (response.ok) {
+            const data = await response.json();
+            setResponseData(data);
+          } else {
+            const errorData = await response.text();
+            setError(errorData);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setError('Failed to fetch data.');
+        }
       };
   
       fetchTestData();
@@ -39,11 +75,15 @@ const TestComponent = () => {
   
     return (
       <div>
-        {responseData && (
+        {error ? (
+          <p>Error: {error}</p>
+        ) : responseData ? (
           <div>
             <p>Test data received from backend:</p>
             <pre>{JSON.stringify(responseData, null, 2)}</pre>
           </div>
+        ) : (
+          <p>Loading...</p>
         )}
       </div>
     );

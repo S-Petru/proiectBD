467f595256fceffcbb229f56d43422a49becee55
S-Petru <slavilapetru@gmail.com>
2023-11-07 19:36:01 +0200

    Added login and register files

--- proiect_bd/src/TestComponent.js
+++ proiect_bd/src/TestComponent.js
@@ -1,60 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-// const getComments = () => {
-//     const config = {
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-  
-//       },
-//       method: "GET",
-//       url: `${url}/all`,
-//       withCredentials: true,
-//     };
-//     return axios(config)
-//       .then(serviceHelper.onGlobalSuccess)
-//       .catch(serviceHelper.onGlobalError);
-//   };
-
-// const TestComponent = () => {
-//     const [responseData, setResponseData] = useState(null);
-  
-//     useEffect(() => {
-// // console.log(`localhost:3001/test`)
-
-//       const fetchTestData = async () => {
-
-//         // fetch("http://localhost:3001/test")
-//         fetch(`${process.env.REACT_APP_API_URL}/test`)
-//             .then(response => response.json())
-//             .then(data => {
-//             console.log('Received JSON:', data);
-// })
-// .catch(error => console.error('Error fetching data:', error));
-
-//       };
-  
-//       fetchTestData();
-//     }, []);
-  
-//     return (
-//       <div>
-//         {responseData && (
-//           <div>
-//             <p>Test data received from backend:</p>
-//             <pre>{JSON.stringify(responseData, null, 2)}</pre>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   };
-
 const TestComponent = () => {
     const [responseData, setResponseData] = useState(null);
     const [error, setError] = useState(null);
   
     useEffect(() => {
-      console.log(`${process.env.REACT_APP_API_URL}/test`)
+      // console.log(`${process.env.REACT_APP_API_URL}/test`);
 
       const fetchTestData = async () => {
         try {
@@ -76,7 +27,7 @@ const TestComponent = () => {
   
       fetchTestData();
 
-      console.log()
+      console.log();
     }, []);
   
     return (
